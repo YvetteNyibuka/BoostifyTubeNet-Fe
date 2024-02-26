db099f67347620e1c852e6bc08b8c04c9adbca5a
YvetteNyibuka <izanyibukayvette@gmail.com>
2024-02-26 21:58:32 +0200

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-center m-auto p-24 bg-neutral-950 ">
+      <h1 className="md:text-6xl md:font-bold md:text-red-600 font-thin text-white text-[2px] xl:text-green-700">
+        Welcome to BoostifyTube Network
+      </h1>
+    </main>
+  );
+}
