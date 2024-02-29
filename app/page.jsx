ae469aaf146a6d426967a121690c4124e2a1605b
YvetteNyibuka <izanyibukayvette@gmail.com>
2024-02-29 10:21:02 +0200

    resolving deployement error

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-center m-auto p-24">
-      {[1, 2, 3, 4, 5, 65].map(() => (
         <h1 className="md:text-6xl md:font-bold md:text-red-600 font-thin text-white text-[2px] xl:text-green-700">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium
           quibusdam corporis tempore placeat. Dicta iure perspiciatis deserunt
           id iste? Cum, veniam laborum labore perspiciatis commodi iusto error
           incidunt! Illo, quae.
         </h1>
-      ))}
     </main>
   );
 }
